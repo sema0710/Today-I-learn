021570de24172066471753052e18e9013cc4000d
sema0710 <pandati@naver.com>
2019-06-19 19:08:47 +0900

    react 설정
    react 설정

--- react/todo-list/src/components/TodoItem.js
+++ react/todo-list/src/components/TodoItem.js
@@ -9,8 +9,10 @@ class TodoItem extends Component {
       <div className="todo-item" onClick={() => onToggle(id)}>
         <div className="remove" onClick={(e) => {
           e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-          onRemove(id)}
-        }>&times;</div>
+              onRemove(id)
+            }
+          }>&times;
+        </div>
         <div className={`todo-text ${checked && 'checked'}`}>
           <div>{text}</div>
         </div>
