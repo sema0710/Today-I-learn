e83d8dde113394070786b3626ecdaac44523b410
sema0710 <pandati@naver.com>
2019-06-24 13:39:11 +0900

    react
    react

--- react/todo-list/src/App.js
+++ react/todo-list/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import './App.css';
+
 class App extends React.Component {
   state = {
     list:[],
     value:"",
     id:0,
   }
+
   onClickHandler = (e)=>{
     let text = document.getElementById("input").value;
+    if(text == ""){
+      alert("plz write what to do");
+      return;
+    }
     this.setState(
       {value:text,
       list : this.state.list.concat({id:this.state.id++,text:text,checked:false})
       });
-      console.log(this.state.list);
   }
+
   onDeleteHandler = (e)=>{
-    console.log(e.target.id);
-    let where = document.querySelector('li');
-    where.forEach =()=>{
-      
-    }
-    console.log(where);
+    let where = document.querySelectorAll('div');
+    where.forEach((a)=>{
+      if(a.id == e.target.id){
+        document.getElementById(a.remove());
+      }
+    })
+  }
+
+  onDivClicked = (e)=>{
+    let where = document.querySelectorAll('div');
+    console.log(e.target);
+    where.forEach((a)=>{
+      if(a.id == e.target.id && this.state.list[e.target.id].checked == false){
+        a.style.opacity = "0.5"
+        this.state.list[e.target.id].checked = true;
+      }
+      else if(a.id == e.target.id && this.state.list[e.target.id].checked == true){
+        this.state.list[e.target.id].checked = false;
+        a.style.opacity = "1.0"
+      }
+    })
   }
   render(){ 
     return (
@@ -32,14 +53,12 @@ class App extends React.Component {
         <div id="test">
           <ul>
           {
-            this.state.list.map((comp,i)=>{
-              return ( 
-              <div>
+            this.state.list.map((comp,i)=>( 
+              <div id={comp.id} onClick={this.onDivClicked} key={i}>
                 <li id={comp.id}>{comp.text}</li>
-                <button onClick={this.onDeleteHandler} id={comp.id}>✔</button>
+                <button onClick={this.onDeleteHandler} id={comp.id}>X</button>
               </div>
-              )
-            })
+              ))
           }
           </ul>
         </div>
